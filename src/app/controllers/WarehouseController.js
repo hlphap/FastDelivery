47b538376f9cc80f415c72b1bfc30e0dc51c2078
PhapHL <longphappy@gmail.com>
2021-06-20 19:53:53 +0700

    RestFullAPI WareHousing

--- src/app/controllers/WarehouseController.js
+++ src/app/controllers/WarehouseController.js
@@ -1,4 +1,6 @@
+const { findByIdAndDelete } = require("../models/Warehouse");
 const Warehouse = require("../models/Warehouse");
+const addressController = require("./AddressController");
 
 class WarehouseController {
   //[GET] /warehouses
@@ -28,27 +30,59 @@ class WarehouseController {
   }
 
   //[POST] /warehouses
-  create(req, res, next) {
+  async create(req, res, next) {
     const formData = req.body;
+    const formAddress = JSON.parse(formData.idAddress);
+
+    //Create address
+    formData.idAddress = await addressController.create(formAddress);
+
     const warehouse = new Warehouse(formData);
     warehouse
       .save()
-      .then(() => res.status(200).json({ result: "1" }))
+      .then(() => res.status(200).json({
+          status: 200,
+          message: "Create WareHouse Success ",
+        }))
       .catch(next);
   }
 
   //[PUT] /warehouses/:id
   update(req, res, next) {
     const formData = req.body;
-    Warehouse.updateOne({ _id: req.params.id }, formData)
-      .then(() => res.status(200).json({ result: "1" }))
+    const formAddress = JSON.parse(formData.idAddress);
+    const updateAddress = addressController.update(
+      formAddress._id,
+      formAddress,
+    );
+    delete formData.idAddress;
+
+    const updateWareHouse = new Promise((resolve, reject) => {
+       Warehouse.updateOne({ _id: req.params.id }, formData)
+      .then(() => resolve())
+      .catch(() => reject());
+    });
+
+    Promise.all([updateAddress, updateWareHouse])
+      .then((result)=>{
+        res.status(200).json({ status: 200, message: "Update success" });
+      })
       .catch(next);
   }
 
   //[DELETE] /warehouses/:id
-  delete(req, res, next) {
+  async delete(req, res, next) {
+    const idAddress = await Warehouse.findById(req.params.id)
+      .then((warehouse) => warehouse.idAddress)
+      .catch(next);
+
+    addressController.delete(idAddress);
+
     Warehouse.deleteOne({ _id: req.params.id })
-      .then(() => res.status(200).json({ result: "1" }))
+      .then(() => res.status(200).json({
+        status: 200,
+        message: "Delete success" }
+      ))
       .catch(next);
   }
 }
