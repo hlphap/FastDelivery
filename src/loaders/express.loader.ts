1116754db9c51a37fe209d8cdd829a4ebfdc8af4
hlphap <longphappy@gmail.com>
2021-12-17 15:20:20 +0700

    fix rules

--- src/loaders/express.loader.ts
+++ src/loaders/express.loader.ts
@@ -34,6 +34,19 @@ export default () => {
     app.use(cors());
     app.options('*', cors);
 
+    /** Rules of API */
+    app.use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+        if (req.method == 'OPTIONS') {
+            res.header('Access-Control-Allow-Methods', 'GET PATCH DELETE PUT POST');
+            return res.status(200).json({});
+        }
+
+        return next();
+    });
+
     // setup limits
     if (env.isProduction) {
         // app.use('/v1/auth', customizeLimiter)
